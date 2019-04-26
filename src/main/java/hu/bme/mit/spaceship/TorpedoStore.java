cdb148ed25d0013c1b05ae1e3f822ceaed18a0bc
aronsimon <simonaron@live.com>
2019-04-26 13:19:07 +0200

    Ofelsege ohajainak teljesitese.

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,7 +9,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
-	private Random generator;
+  private Random generator;
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
@@ -18,7 +18,7 @@ public class TorpedoStore {
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
-	generator = new Random();
+    generator = new Random();
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
@@ -39,7 +39,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    //Random generator = new Random();
+
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
